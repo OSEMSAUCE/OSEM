dc2d2dd57666be904b762e6e39d2c5045a2d34bd
Chris Harris <christobinharris@gmail.com>
2025-07-30 15:02:45 -0700

    feat: update favicon with new logo design

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import logo from '$lib/images/svelte-logo.svg';
+	import logo from '$lib/images/favicon.svg';
 	import github from '$lib/images/github.svg';
 </script>
 
