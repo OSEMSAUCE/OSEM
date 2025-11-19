242a69c24f8627f983aee37f51fc73d88a15a366
Chris Harris <christobinharris@gmail.com>
2025-11-19 15:05:27 -0800

    deleted more old stuff

--- src/lib/types/subscriptionDetails.ts
+++
@@ -1,8 +0,0 @@
-import type { Product, ProductVariant } from "$lib/server/database/schemas/productsSchema";
-import type { Subscription } from "$lib/server/database/schemas/subscriptionSchema";
-
-export type SubscriptionDetails = {
-    subscription: Subscription;
-    product: Product;
-    product_variant: ProductVariant;
-}
