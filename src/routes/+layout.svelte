31c8025f9d92e84f40e230591301bf5f5859cb00
Chris Harris <christobinharris@gmail.com>
2025-11-25 13:47:58 -0800

    chore: Disable ModeWatcher component and remove the dark class from the html tag.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script>
 	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
+	// dark mode here!
+	// import { ModeWatcher } from 'mode-watcher';
 	import Navbar from '$lib/components/layout/navbar.svelte';
 	import Footer from '$lib/components/layout/footer.svelte';
 
 	let { children } = $props();
 </script>
 
-<ModeWatcher defaultMode="light" />
+<!-- dark mode here! -->
+<!-- <ModeWatcher defaultMode="light" track={false} /> -->
 
 <Navbar />
 
