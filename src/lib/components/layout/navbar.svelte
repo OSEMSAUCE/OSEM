95e14d1519df8b3350c9d1f9be77e3c075e9bfdb
Chris Harris <christobinharris@gmail.com>
2025-12-18 09:12:28 -0800

    feat: add ReTreever origins to CORS configuration and refactor API and UI import paths

--- src/lib/components/layout/navbar.svelte
+++ src/lib/components/layout/navbar.svelte
@@ -3,7 +3,7 @@ import { Button } from '../../subwoof/components/ui/button';
 import { page } from '$app/state';
 import Icon from '@iconify/svelte';
 import * as Sheet from '../../subwoof/components/ui/sheet/index.js';
-import { cn } from '../../subwoof/utils';
+import { cn } from '../../utils';
 
 	const menu = [
 		{
