4a0f32e52eef5a6f3a43b64a1b20f1ae2727f424
Chris Harris <christobinharris@gmail.com>
2026-03-19 16:48:58 -0700

    Update PrismaClient import paths in score_orgs.ts and score_projects.ts to use the new generated folder structure

--- score_scripts/score_projects.ts
+++ score_scripts/score_projects.ts
@@ -5,7 +5,7 @@ import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
-import { PrismaClient } from "../../src/lib/generated/prisma-postgres/client.js";
+import { PrismaClient } from "../../prisma/generated/prisma-postgres/client.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
