671db7097a73b3ed9ea1afd4593c1aa8ca671a74
Chris Harris <christobinharris@gmail.com>
2026-03-24 21:25:00 -0700

    Refactor navbar to dynamically switch between mobile and desktop menu based on the current page's URL

--- src/routes/mobile/+layout.svelte
+++ src/routes/mobile/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import { Toaster } from '$lib/components/ui/sonner';
-	import MobileNav from '$lib/components/mobMap/MobileNav.svelte';
-
 	let { children } = $props();
 </script>
 
 <Toaster position="top-center" />
-<div style="--subnav-height: 2.5rem">
-	<MobileNav />
-	{@render children()}
-</div>
+{@render children()}
