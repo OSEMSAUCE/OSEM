0d41828f84b2e2301498ad85071d78f87224becc
Chris Harris <christobinharris@gmail.com>
2026-04-28 00:45:33 -0700

    Increase SVG and image dimensions from 36px to 40px in mapFeaturePopover.svelte

--- src/lib/components/map/mapParts/mapFeaturePopover.svelte
+++ src/lib/components/map/mapParts/mapFeaturePopover.svelte
@@ -132,7 +132,7 @@ let style = $derived.by(() => {
                     onclick={() => onChangeIcon?.(pt.key)}
                 >
                     {#if pt.key === "default"}
-                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="#ffd700" stroke="#ffd700" stroke-width="0.5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="#ffd700" stroke="#ffd700" stroke-width="0.5">
                             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"/>
                         </svg>
                     {:else}
@@ -200,8 +200,8 @@ let style = $derived.by(() => {
         background: rgba(255, 215, 0, 0.15);
     }
     .fp-swatch-img {
-        width: 36px;
-        height: 36px;
+        width: 40px;
+        height: 40px;
         object-fit: contain;
         display: block;
     }
